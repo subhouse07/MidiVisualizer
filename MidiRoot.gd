de08c936e04d1241fa76a22b060a008227917437
andy <subhouse07@gmail.com>
2022-11-22 13:55:27 -0600

    Get sysex status light working

--- MidiRoot.gd
+++ MidiRoot.gd
@@ -1,25 +1,31 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var sysex_on = true
+var xg_on = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	OS.open_midi_inputs()
-	var midi_inputs = OS.get_connected_midi_inputs()
-	for i in midi_inputs:
-		print(i)
+	$SysexReceiver.is_sysex = true
+	$SysexReceiver.status_on()
+	$XGReceiver.stay_off()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _unhandled_input(event):
 	if event is InputEventMIDI:
 		match event.message:
 			MIDI_MESSAGE_NOTE_ON:
+				if sysex_on:
+					sysex_on = false
+					$SysexReceiver.stay_off()
+					$XGReceiver.stay_on()
 				$NoteReceiver.status_on()
 				$ChannelBars.get_child(event.channel).note_on(event.pitch, event.velocity)
 			MIDI_MESSAGE_NOTE_OFF:
 				$ChannelBars.get_child(event.channel).note_off(event.pitch)
+			MIDI_MESSAGE_SYSTEM_EXCLUSIVE:
+				print("Start received")
+
+# Called when going to the next track
+func reset():
+	sysex_on = true
+	$SysexReceiver.status_on()
--- StatusReceiver.gd
+++ StatusReceiver.gd
@@ -5,7 +5,12 @@ extends Node2D
 var on_texture = preload("res://img/status_on.png")
 var off_texture = preload("res://img/status_off.png")
 var cooldown = 0
+var reset_timer = 0 
+var is_sysex = false
 
+signal led_timeout
+
+const RESET = 10
 const TIMEOUT = 10
 
 
@@ -20,7 +25,23 @@ func _process(delta):
 		cooldown -= 1
 		if cooldown == 0:
 			$Sprite.texture = off_texture
+			if (is_sysex):
+				reset_timer = RESET
+	elif reset_timer > 0:
+		reset_timer -= 1
+		if reset_timer == 0:
+			$Sprite.texture = on_texture
+			cooldown = TIMEOUT
 
 func status_on():
 	$Sprite.texture = on_texture
 	cooldown = TIMEOUT
+
+func stay_on():
+	cooldown = -1
+	$Sprite.texture = on_texture
+
+func stay_off():
+	cooldown = -1
+	reset_timer = -1
+	$Sprite.texture = off_texture
